1ce34e61b55c251c591909a40e8057856a1fdfc6
Leah Chercham <leahchercham@hotmail.com>
2019-09-18 10:30:28 -0500

    basic rendering

--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,27 @@
 
 const logIn = function () {
+    //debugger
     const username = $("#username").val()
     let password = $("#password").val()
     if(password){
-    $.post('/login', { username }, function (u) {
+    //$.post('/login', { username }, function (u) {
         user = new User(username)
-        render(LOGIN_TEMPLATE, { isLoggedIn: true, username }, LOGIN_AREA)
+        renderer.render(LOGIN_TEMPLATE, { isLoggedIn: true, username }, LOGIN_AREA)
+        let data = "" // because we need data to use handlebars
+        renderer.render(MAIN_TEMPLATE, data, MAIN_AREA)
+        renderer.render(MAIN_TEMPLATE, data, MAIN_AREA)
+        renderer.render(DB_TEMPLATE, data, DB_AREA)
+
         //render(MAIN_TEMPLATE, , MAIN_AREA)
-    })} else {
+    } else {
         alert("Please enter your password.")
     }
 }
 
 const logOut = function(){
     user = undefined
-    render(LOGIN_TEMPLATE, { isLoggedIn: false }, LOGIN_AREA)
+    renderer.render(LOGIN_TEMPLATE, { isLoggedIn: false }, LOGIN_AREA)
     renderer.clear(MAIN_AREA)
 }
 
-render(LOGIN_TEMPLATE, { isLoggedIn: false }, LOGIN_AREA)
+renderer.render(LOGIN_TEMPLATE, { isLoggedIn: false }, LOGIN_AREA)
