c370e6a0d783b0d47ef75bb2b09c6ca68c2978e7
Leah Chercham <leahchercham@hotmail.com>
2019-09-18 16:15:25 -0500

    working on save functions

--- dist/classes/DataManager.js
+++ dist/classes/DataManager.js
@@ -17,7 +17,6 @@ class DataManager {
         console.log(this.savedPodcasts)
     }
 
-
     async getPodcastFromAPI(skill) {
         let response = await $.get(`/podcasts/${skill}`)
         if (response) {
@@ -30,5 +29,18 @@ class DataManager {
         console.log(this.podcasts)
         // v4 await getCourseFromAPI()
         // v3 await getBookFromAPI()
+    }
+
+    saveToDB(object, user){
+        return $.ajax({
+            method:"put",
+            url:`/podcast/${user}`,
+            data: object,
+            success: ()=>{
+                 this.getAllDataFromDB()
+            },
+            error: function(xhr,text,error){console.log("error : "+ error + " - " + text)}
+        })
+
     }
 }
--- dist/classes/UserClass.js
+++ dist/classes/UserClass.js
@@ -8,6 +8,14 @@ class User {
         let data = {podcast: dataManager.podcasts }
         console.log("this is data: " + data)
         renderer.render(RESULTS_TEMPLATE, data, RESULTS_AREA)
-        
+    }
+
+    saveToDB(podcast){
+        let user = this.name
+        dataManager.saveToDB(podcast, user).then(()=>{
+            let data = dataManager.savedPodcasts
+            console.log(data)
+            renderer.render(DB_TEMPLATE, data, DB_AREA)
+        })
     }
 }
