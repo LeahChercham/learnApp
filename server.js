472966eaf213d2d5facb6ecab40380c4ffe184a7
Simona Bali <simona.bali@outlook.com>
2019-09-19 08:51:36 -0500

    Heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 
 const api = require('./Server/routes/api')
 
-mongoose.connect('mongodb://localhost/usersDB', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/usersDB', { useNewUrlParser: true })
 
 const server = express()
 
@@ -18,5 +18,5 @@ server.use(express.static(path.join(__dirname, 'node_modules')))
 
 server.use('/', api)
 
-const port = '3000';
-server.listen(port, function () { console.log('Running on ' + port) })
+const PORT = '3000';
+server.listen(process.env.PORT || PORT, function () { console.log('Running on ' + port) })
